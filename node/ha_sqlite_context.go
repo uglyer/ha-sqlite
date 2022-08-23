c4827e406016ab64f5f9559671214c0a260d9945
uglyer <root@wanda.sb>
2022-08-23 17:26:53 +0800

    Implement remote call process

--- node/ha_sqlite_context.go
+++ node/ha_sqlite_context.go
@@ -3,7 +3,6 @@ package node
 import (
 	"context"
 	"fmt"
-	"github.com/Jille/raft-grpc-leader-rpc/leaderhealth"
 	"github.com/hashicorp/raft"
 	"github.com/uglyer/ha-sqlite/db"
 	"github.com/uglyer/ha-sqlite/proto"
@@ -15,6 +14,7 @@ import (
 
 type HaSqliteContext struct {
 	// Config 配置参数
+	Ctx    context.Context
 	Config *HaSqliteConfig
 	fsm    *db.HaSqliteRaftFSM
 	Raft   *raft.Raft
@@ -32,22 +32,56 @@ func NewHaSqliteContext(config *HaSqliteConfig) (*HaSqliteContext, error) {
 	if err != nil {
 		return nil, err
 	}
-	s := grpc.NewServer()
-	tm.Register(s)
-	leaderhealth.Setup(r, s, []string{"Example"})
-	reflection.Register(s)
-	if err := s.Serve(sock); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
 	c := &HaSqliteContext{
+		Ctx:    ctx,
 		Config: config,
 		fsm:    fsm,
 		Raft:   r,
+	}
+	if config.JoinAddress != "" {
+		log.Printf("start join %v", config.JoinAddress)
+		resp, err := c.CallRemoteJoin(config.JoinAddress)
+		if err != nil {
+			c.Raft.Shutdown()
+			return nil, fmt.Errorf("failed to join: %v,%v", resp, err)
+		}
+		log.Println(resp)
+	} else {
+		log.Printf("with out JoinAddress, skip join")
+	}
+	s := grpc.NewServer()
+	tm.Register(s)
+	//leaderhealth.Setup(r, s, []string{"Example"})
+	reflection.Register(s)
+	if err := s.Serve(sock); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 	proto.RegisterHaSqliteInternalServer(s, c)
 	return c, nil
 }
 
 func (ctx *HaSqliteContext) Join(c context.Context, req *proto.JoinRequest) (*proto.JoinResponse, error) {
-	return nil, fmt.Errorf("TODO impl Join")
+	log.Printf("remote call Join:%v", req)
+	return &proto.JoinResponse{
+		Code:    proto.ResultCode_NOT_A_LEADER,
+		Message: proto.ResultCode_NOT_A_LEADER.String(),
+		Index:   ctx.Raft.LastIndex(),
+	}, fmt.Errorf("TODO impl Join")
+}
+
+func (ctx *HaSqliteContext) CallRemoteJoin(remoteAddress string) (*proto.JoinResponse, error) {
+	// TODO 远程调用存在问题
+	req := &proto.JoinRequest{
+		Id:            ctx.Config.RaftId,
+		Address:       ctx.Config.Address,
+		PreviousIndex: ctx.Raft.LastIndex(),
+	}
+	var o grpc.DialOption = grpc.EmptyDialOption{}
+	conn, err := grpc.Dial(remoteAddress, grpc.WithInsecure(), grpc.WithBlock(), o)
+	if err != nil {
+		return nil, fmt.Errorf("CallRemoteJoin open conn error: %v", err)
+	}
+	defer conn.Close()
+	client := proto.NewHaSqliteInternalClient(conn)
+	return client.Join(ctx.Ctx, req)
 }
