7600a8391be54373994400e17eeb4573a57ecdb7
uglyer <root@wanda.sb>
2022-08-24 18:08:40 +0800

    创建 fsm 节点

--- db/ha_sqlite_raft_fsm.go
+++ db/ha_sqlite_raft_fsm.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/hashicorp/raft"
 	"io"
+	"log"
 	"sync"
 )
 
@@ -13,18 +14,25 @@ type HaSqliteRaftFSM struct {
 
 //var _ raft.FSM = &HaSqliteRaftFSM{}
 
+func NewHaSqliteRaftFSM() *HaSqliteRaftFSM {
+	return &HaSqliteRaftFSM{}
+}
+
 func (h *HaSqliteRaftFSM) Apply(l *raft.Log) interface{} {
+	log.Println("HaSqliteRaftFSM.Apply")
 	h.mtx.Lock()
 	defer h.mtx.Unlock()
 	return nil
 }
 
 func (h *HaSqliteRaftFSM) Snapshot() (raft.FSMSnapshot, error) {
+	log.Println("HaSqliteRaftFSM.Snapshot")
 	// Make sure that any future calls to f.Apply() don't change the snapshot.
 	return &HaSqliteSnapshot{}, nil
 }
 
 func (h *HaSqliteRaftFSM) Restore(r io.ReadCloser) error {
+	log.Println("HaSqliteRaftFSM.Restore")
 
 	return nil
 }
