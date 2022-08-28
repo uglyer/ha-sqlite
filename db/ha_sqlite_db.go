eff40a19a945b3379f344ef7c36d2b81718b4686
unily <root@wanda.sb>
2022-08-28 09:48:19 +0800

    实现执行函数.

--- db/ha_sqlite_db.go
+++ db/ha_sqlite_db.go
@@ -3,9 +3,11 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/pkg/errors"
 	"github.com/uglyer/ha-sqlite/proto"
 	"sync"
+	"time"
 )
 
 type HaSqliteDB struct {
@@ -40,4 +42,85 @@ func (d *HaSqliteDB) Open(c context.Context, req *proto.OpenRequest) (*proto.Ope
 	d.dbFilenameTokenMap[req.Dsn] = token
 	d.dbMap[token] = db
 	return &proto.OpenResponse{DbId: token}, nil
+}
+
+// Exec 执行数据库命令
+func (d *HaSqliteDB) Exec(c context.Context, req *proto.ExecRequest) (*proto.ExecResponse, error) {
+	d.mtx.Lock()
+	defer d.mtx.Unlock()
+	db, ok := d.dbMap[req.Request.DbId]
+	if !ok {
+		return nil, fmt.Errorf("get db error : %d", req.Request.DbId)
+	}
+	var allResults []*proto.ExecResult
+
+	// handleError sets the error field on the given result. It returns
+	// whether the caller should continue processing or break.
+	handleError := func(result *proto.ExecResult, err error) bool {
+		result.Error = err.Error()
+		allResults = append(allResults, result)
+		return true
+	}
+
+	// Execute each statement.
+	for _, stmt := range req.Request.Statements {
+		ss := stmt.Sql
+		if ss == "" {
+			continue
+		}
+
+		result := &proto.ExecResult{}
+		start := time.Now()
+
+		parameters, err := parametersToValues(stmt.Parameters)
+		if err != nil {
+			if handleError(result, err) {
+				continue
+			}
+			break
+		}
+
+		r, err := db.ExecContext(c, ss, parameters...)
+		if err != nil {
+			if handleError(result, err) {
+				continue
+			}
+			break
+		}
+
+		if r == nil {
+			continue
+		}
+
+		lid, err := r.LastInsertId()
+		if err != nil {
+			if handleError(result, err) {
+				continue
+			}
+			break
+		}
+		result.LastInsertId = lid
+
+		ra, err := r.RowsAffected()
+		if err != nil {
+			if handleError(result, err) {
+				continue
+			}
+			break
+		}
+		result.RowsAffected = ra
+		if req.Timings {
+			result.Time = time.Now().Sub(start).Seconds()
+		}
+		allResults = append(allResults, result)
+	}
+
+	return &proto.ExecResponse{
+		Result: allResults,
+	}, nil
+}
+
+// Query 查询记录
+func (d *HaSqliteDB) Query(c context.Context, req *proto.QueryRequest) (*proto.QueryResponse, error) {
+	return nil, fmt.Errorf("todo db Query")
 }
---
+++ db/ha_sqlite_db_tool.go
@@ -0,0 +1,126 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/uglyer/ha-sqlite/proto"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// parametersToValues maps values in the proto params to SQL driver values.
+func parametersToValues(parameters []*proto.Parameter) ([]interface{}, error) {
+	if parameters == nil {
+		return nil, nil
+	}
+
+	values := make([]interface{}, len(parameters))
+	for i := range parameters {
+		switch w := parameters[i].GetValue().(type) {
+		case *proto.Parameter_I:
+			values[i] = sql.Named(parameters[i].GetName(), w.I)
+		case *proto.Parameter_D:
+			values[i] = sql.Named(parameters[i].GetName(), w.D)
+		case *proto.Parameter_B:
+			values[i] = sql.Named(parameters[i].GetName(), w.B)
+		case *proto.Parameter_Y:
+			values[i] = sql.Named(parameters[i].GetName(), w.Y)
+		case *proto.Parameter_S:
+			values[i] = sql.Named(parameters[i].GetName(), w.S)
+		default:
+			return nil, fmt.Errorf("unsupported type: %T", w)
+		}
+	}
+	return values, nil
+}
+
+// normalizeRowValues performs some normalization of values in the returned rows.
+// Text values come over (from sqlite-go) as []byte instead of strings
+// for some reason, so we have explicitly converted (but only when type
+// is "text" so we don't affect BLOB types)
+func normalizeRowValues(row []interface{}, types []string) ([]*proto.Parameter, error) {
+	values := make([]*proto.Parameter, len(types))
+	for i, v := range row {
+		switch val := v.(type) {
+		case int:
+		case int64:
+			values[i] = &proto.Parameter{
+				Value: &proto.Parameter_I{
+					I: val,
+				},
+			}
+		case float64:
+			values[i] = &proto.Parameter{
+				Value: &proto.Parameter_D{
+					D: val,
+				},
+			}
+		case bool:
+			values[i] = &proto.Parameter{
+				Value: &proto.Parameter_B{
+					B: val,
+				},
+			}
+		case string:
+			values[i] = &proto.Parameter{
+				Value: &proto.Parameter_S{
+					S: val,
+				},
+			}
+		case []byte:
+			if isTextType(types[i]) {
+				values[i].Value = &proto.Parameter_S{
+					S: string(val),
+				}
+			} else {
+				values[i] = &proto.Parameter{
+					Value: &proto.Parameter_Y{
+						Y: val,
+					},
+				}
+			}
+		case time.Time:
+			rfc3339, err := val.MarshalText()
+			if err != nil {
+				return nil, err
+			}
+			values[i] = &proto.Parameter{
+				Value: &proto.Parameter_S{
+					S: string(rfc3339),
+				},
+			}
+		case nil:
+			continue
+		default:
+			return nil, fmt.Errorf("unhandled column type: %T %v", val, val)
+		}
+	}
+	return values, nil
+}
+
+// isTextType returns whether the given type has a SQLite text affinity.
+// http://www.sqlite.org/datatype3.html
+func isTextType(t string) bool {
+	return t == "text" ||
+		t == "json" ||
+		t == "" ||
+		strings.HasPrefix(t, "varchar") ||
+		strings.HasPrefix(t, "varying character") ||
+		strings.HasPrefix(t, "nchar") ||
+		strings.HasPrefix(t, "native character") ||
+		strings.HasPrefix(t, "nvarchar") ||
+		strings.HasPrefix(t, "clob")
+}
+
+func randomString() string {
+	var output strings.Builder
+	chars := "abcdedfghijklmnopqrstABCDEFGHIJKLMNOP"
+
+	for i := 0; i < 20; i++ {
+		random := rand.Intn(len(chars))
+		randomChar := chars[random]
+		output.WriteString(string(randomChar))
+	}
+	return output.String()
+}
