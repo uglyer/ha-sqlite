ff60e7af1784fd96e71089aba46f9d329d35dd59
uglyer <root@wanda.sb>
2022-08-30 22:38:21 +0800

    单元测试: 单节点增加关闭节点流程,调整断言实现.

--- node/ha_sqlite_context_test.go
+++ node/ha_sqlite_context_test.go
@@ -1,8 +1,10 @@
 package node_test
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	_ "github.com/uglyer/ha-sqlite/driver"
 	"github.com/uglyer/ha-sqlite/node"
 	"github.com/uglyer/ha-sqlite/proto"
@@ -12,13 +14,15 @@ import (
 	"path/filepath"
 	"runtime"
 	"testing"
+	"time"
 )
 
 type Node struct {
-	ctx *node.HaSqliteContext
+	ctx    *node.HaSqliteContext
+	config *node.HaSqliteConfig
 }
 
-func NewNode(config *node.HaSqliteConfig, deleteLog bool) (*Node, error) {
+func NewNode(t *testing.T, config *node.HaSqliteConfig, deleteLog bool) *Node {
 	if deleteLog {
 		baseDir := filepath.Join(config.DataPath, config.RaftId)
 		err := os.RemoveAll(baseDir)
@@ -27,93 +31,88 @@ func NewNode(config *node.HaSqliteConfig, deleteLog bool) (*Node, error) {
 		}
 	}
 	_, port, err := net.SplitHostPort(config.Address)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse local address (%q): %v", config.Address, err)
-	}
+	assert.Nilf(t, err, "failed to parse local address (%q): %v", config.Address, err)
 	config.LocalPort = port
 	ctx, err := node.NewHaSqliteContext(config)
-
-	if err != nil {
-		log.Fatalf("failed to start HaSqliteContext: %v", err)
-	}
+	assert.Nilf(t, err, "failed to start HaSqliteContext: %v", err)
 	return &Node{
-		ctx: ctx,
-	}, nil
+		ctx:    ctx,
+		config: config,
+	}
 }
 
 func (store *Node) Serve() {
-	defer store.ctx.Sock.Close()
-	if err := store.ctx.GrpcServer.Serve(store.ctx.Sock); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	store.ctx.GrpcServer.Serve(store.ctx.Sock)
+}
+
+func (store *Node) Stop() {
+	store.ctx.Sock.Close()
+	store.ctx.Raft.Shutdown()
 }
 
 type HaDB struct {
-	db *sql.DB
-	t  *testing.T
+	Store *Node
+	db    *sql.DB
+	t     *testing.T
 }
 
-func openSingleNodeDB(t *testing.T, deleteLog bool) *HaDB {
-	store, err := NewNode(&node.HaSqliteConfig{
-		Address:       "localhost:30333",
+func openSingleNodeDB(t *testing.T, port int, nodeId string, deleteLog bool) *HaDB {
+	store := NewNode(t, &node.HaSqliteConfig{
+		Address:       fmt.Sprintf("localhost:%d", port),
 		RaftBootstrap: true,
-		RaftId:        "nodeTestA",
+		RaftId:        nodeId,
 		DataPath:      "data",
 		RaftAdmin:     true,
 	}, deleteLog)
-	if err != nil {
-		t.Fatalf("启动rpc服务失败:%v", err)
-	}
 	go func() {
 		store.Serve()
 	}()
 	store.ctx.WaitHasLeader()
-	db, err := sql.Open("ha-sqlite", "multi:///localhost:30333/:memory:")
-	if err != nil {
-		t.Fatalf("ha-sqlite open error:%v", err)
-	}
+	url := fmt.Sprintf("multi:///localhost:%d/:memory:", port)
+	db, err := sql.Open("ha-sqlite", url)
+
+	assert.Nilf(t, err, "ha-sqlite open error:%v", err)
 	db.SetMaxIdleConns(runtime.NumCPU() * 2)
 	db.SetMaxOpenConns(runtime.NumCPU() * 2)
-	err = db.Ping()
-	if err != nil {
-		t.Fatalf("ha-sqlite ping error:%v", err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*800)
+	defer cancel()
+	go func() {
+		err = db.PingContext(ctx)
+		assert.Nilf(t, err, "ha-sqlite ping error:%v", err)
+	}()
+	select {
+	case <-ctx.Done():
+		return &HaDB{db: db, Store: store, t: t}
+	case <-time.After(time.Millisecond * 900):
+		t.Fatalf("connect %s timeout", url)
+		return &HaDB{db: db, t: t}
 	}
-	return &HaDB{db: db, t: t}
 }
 
 func (store *HaDB) assertExec(sql string, args ...interface{}) {
 	_, err := store.db.Exec(sql, args...)
-	if err != nil {
-		store.t.Fatalf("Error exec:%v", err)
-	}
+	assert.Nilf(store.t, err, "Error exec:%v", err)
 }
 
 func (store *HaDB) assertExecCheckEffect(target *proto.ExecResult, sql string, args ...interface{}) {
 	result, err := store.db.Exec(sql, args...)
-	if err != nil {
-		store.t.Fatalf("Error exec:%v", err)
-	}
+	assert.Nilf(store.t, err, "Error exec:%v", err)
 	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		store.t.Fatalf("fail get RowsAffected:%v", err)
-	}
+	assert.Nilf(store.t, err, "fail get RowsAffected:%v", err)
 	lastInsertId, err := result.LastInsertId()
-	if err != nil {
-		store.t.Fatalf("fail get LastInsertId:%v", err)
-	}
-	if rowsAffected != target.RowsAffected {
-		store.t.Fatalf("预期的RowsAffected不一致，期望：%d,实际：%d，sql: %s", target.RowsAffected, rowsAffected, sql)
-	} else if lastInsertId != target.LastInsertId {
-		store.t.Fatalf("预期的LastInsertId不一致，期望：%d,实际：%d，sql: %s", target.LastInsertId, lastInsertId, sql)
-	}
+	assert.Nilf(store.t, err, "fail get LastInsertId:%v", err)
+	assert.Equal(store.t, target.RowsAffected, rowsAffected)
+	assert.Equal(store.t, target.LastInsertId, lastInsertId)
 }
 
-func Test_OpenDB(t *testing.T) {
-	openSingleNodeDB(t, true)
+func Test_SingleNodOpenDB(t *testing.T) {
+	db := openSingleNodeDB(t, 31300, "Test_OpenDB", true)
+	defer db.Store.Stop()
 }
 
-func Test_Exec(t *testing.T) {
-	db := openSingleNodeDB(t, true)
+func Test_SingleNodExec(t *testing.T) {
+	db := openSingleNodeDB(t, 31300, "Test_Exec", true)
+	defer db.Store.Stop()
 	db.assertExec("CREATE TABLE foo (id integer not null primary key, name text)")
 	db.assertExecCheckEffect(&proto.ExecResult{RowsAffected: 1, LastInsertId: 1},
 		"INSERT INTO foo(name) VALUES(?)", "test1")
