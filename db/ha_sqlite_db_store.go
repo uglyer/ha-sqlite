dd6d0c994a9f4896181cf03078c46b2572eb213b
uglyer <root@wanda.sb>
2022-09-19 22:20:38 +0800

    新增 数仓构建方法.

--- db/ha_sqlite_db_store.go
+++ db/ha_sqlite_db_store.go
@@ -1,6 +1,8 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 // HaSqliteDBStore 使用系统信息管理 db(memory or disk)
 // 用于存放dsn、dbId、本地文件路径、拉取状态(本地、S3远端)、版本号、最后一次更新时间、最后一次查询时间、快照版本 等信息
@@ -15,4 +17,21 @@ type HaSqliteDBStoreDBItem struct {
 	SnapshotVersion uint64 `sqlite:"snapshot_version"`
 	CreateTime      int64  `sqlite:"create_time"`
 	UpdateTime      int64  `sqlite:"update_time"`
+}
+
+const dbName = "ha_sqlite"
+
+// NewHaSqliteDBStore 创建新的数仓，会自动创建数据库
+func NewHaSqliteDBStore() (*HaSqliteDBStore, error) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxIdleConns(1)
+	db.SetMaxOpenConns(1)
+	_, err = db.Exec("create table ? (id integer not null primary key, path text, db_version text,snapshot_version text,create_time integer,update_time integer)", dbName)
+	if err != nil {
+		return nil, err
+	}
+	return &HaSqliteDBStore{db: db}, nil
 }
