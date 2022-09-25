64553c1b3b17b928951508be331b4f702270f289
uglyer <root@wanda.sb>
2022-09-25 21:10:19 +0800

    内存文件系统 分页溢出测试.

--- db/memfs/memfs_test.go
+++ db/memfs/memfs_test.go
@@ -2,6 +2,7 @@ package memfs
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math"
 	"os"
 	"testing"
 )
@@ -21,4 +22,33 @@ func TestMemFS(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int64(len(body)), info.Size())
 
+	// 完整读取测试
+	readBody5 := make([]byte, len(body)+2)
+	readCount, err := testFile.ReadAt(readBody5, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, len(body), readCount)
+	assert.Equal(t, body, readBody5[:readCount])
+
+	// 分页测试
+	pageSize := 8192
+	pageBody := make([]byte, pageSize)
+	for i := 0; i < pageSize; i++ {
+		pageBody[i] = byte(math.Round(1.0) * 255)
+	}
+	writeCount, err = testFile.WriteAt(pageBody, int64(len(body)))
+	assert.NoError(t, err)
+	assert.Equal(t, len(pageBody), writeCount)
+
+	info, err = testFile.Stat()
+	assert.NoError(t, err)
+	assert.Equal(t, int64(len(body)+len(pageBody)), info.Size())
+
+	// 分片读取测试
+	readCount, err = testFile.ReadAt(readBody5, int64(len(body)-1))
+	assert.NoError(t, err)
+	assert.Equal(t, len(readBody5), readCount)
+	assert.Equal(t, body[len(body)-1], readBody5[0])
+	for i := 1; i < readCount; i++ {
+		assert.Equal(t, pageBody[i-1], readBody5[i])
+	}
 }
