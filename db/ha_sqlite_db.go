7048bf591f736aeccba820e01cde146473d3ace2
uglyer <root@wanda.sb>
2022-09-25 21:52:49 +0800

    调整 初始化数据库参数设定方式. TODO 启用 vfs 后与 wal 冲突

--- db/ha_sqlite_db.go
+++ db/ha_sqlite_db.go
@@ -40,7 +40,7 @@ func init() {
 		},
 	})
 	vfs = NewHaSqliteVFS()
-	err := sqlite.VFSRegister("demo", vfs)
+	err := sqlite.VFSRegister("ha_sqlite", vfs)
 	if err != nil {
 		panic(fmt.Sprintf("VFSRegister error:%v", err))
 	}
@@ -48,21 +48,14 @@ func init() {
 
 func newHaSqliteDB(dataSourceName string) (*HaSqliteDB, error) {
 	// TODO 实现 VFS https://github.com/psanford/sqlite3vfs github.com/blang/vfs/memfs
-	url := fmt.Sprintf("%s?_txlock=exclusive&_busy_timeout=30000", dataSourceName)
+	// TODO 启用 vfs 后与 wal 冲突
+	url := fmt.Sprintf("%s?_txlock=exclusive&_busy_timeout=30000&_journal_mode=wal&vfs=ha_sqlite&_synchronous=OFF", dataSourceName)
 	db, err := sql.Open("sqlite3-wal", url)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to open database NewHaSqliteDB")
 	}
 	db.SetMaxIdleConns(1)
 	db.SetMaxOpenConns(1)
-	_, err = db.Exec("PRAGMA synchronous = OFF")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to set NewHaSqliteDB synchronous off")
-	}
-	_, err = db.Exec("PRAGMA journal_mode = wal")
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to set NewHaSqliteDB journal_mode MEMORY")
-	}
 
 	sourceWalFile := fmt.Sprintf("%s-wal", dataSourceName)
 	return &HaSqliteDB{
