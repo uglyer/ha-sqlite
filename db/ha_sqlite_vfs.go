d089e681be17796e9845804a19afc0830a006756
uglyer <root@wanda.sb>
2022-10-01 22:32:05 +0800

    wal 文件转发至 memfs.

--- db/ha_sqlite_vfs.go
+++ db/ha_sqlite_vfs.go
@@ -6,6 +6,7 @@ import (
 	"github.com/uglyer/ha-sqlite/db/memfs"
 	"log"
 	"os"
+	"strings"
 	"sync/atomic"
 )
 
@@ -25,19 +26,6 @@ func NewHaSqliteVFS() *HaSqliteVFS {
 }
 
 func (vfs *HaSqliteVFS) Open(name string, flags sqlite3.OpenFlag) (sqlite3.File, sqlite3.OpenFlag, error) {
-	//log.Printf("vfs.open:%s", name)
-	// TODO vfs 实现 sqlite3_io_methods 中 xTruncate,xShmMap,xShmLock,xShmBarrier,xShmUnmap,xFetch,xUnfetch 以支持 wal 模式
-	//if strings.HasSuffix(name, "-wal") {
-	//	file, err := v.rootMemFS.OpenFile(name, flags, 0600)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	return file, nil
-	//}
-	var (
-		f   *os.File
-		err error
-	)
 	log.Printf("vfs.open:%s", name)
 	if name == "" {
 		return nil, flags, fmt.Errorf("")
@@ -55,6 +43,17 @@ func (vfs *HaSqliteVFS) Open(name string, flags sqlite3.OpenFlag) (sqlite3.File,
 	if flags&sqlite3.OpenReadWrite != 0 {
 		fileFlags |= os.O_RDWR
 	}
+	if strings.HasSuffix(name, "-wal") {
+		file, err := vfs.rootMemFS.OpenFile(name, fileFlags, 0600)
+		if err != nil {
+			return nil, 0, err
+		}
+		return file, 0, nil
+	}
+	var (
+		f   *os.File
+		err error
+	)
 	f, err = os.OpenFile(name, fileFlags, 0600)
 	if err != nil {
 		return nil, 0, sqlite3.CantOpenError
