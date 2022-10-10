ec322bc0ed5906e468be36f00268251bdaebf732
uglyer <root@wanda.sb>
2022-10-10 16:32:27 +0800

    🧪 未创建 wal 文件的应用日志单元测试

--- db/walfs/walfs_test.go
+++ db/walfs/walfs_test.go
@@ -2,7 +2,10 @@ package walfs
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/uglyer/ha-sqlite/proto"
+	gProto "google.golang.org/protobuf/proto"
 	"math/rand"
+	"os"
 	"testing"
 )
 
@@ -20,16 +23,50 @@ func TestVfsChecksum(t *testing.T) {
 }
 
 func TestVfsWalApplyLog(t *testing.T) {
-	wal := NewWalFS()
+	walFS := NewWalFS()
 	pageSize := 4096
 	buffer := make([]byte, pageSize)
 	for i := 0; i < pageSize; i++ {
 		buffer[i] = uint8(rand.Uint32())
 	}
+	cmd := &proto.WalCommand{
+		Frames: []*proto.WalFrame{
+			{
+				PageNumber: 1,
+				Data:       buffer,
+			},
+		},
+	}
+
+	logBuffer, err := gProto.Marshal(cmd)
+	assert.NoError(t, err)
 
 	t.Run("without wal file test", func(t *testing.T) {
-		err := wal.VfsApplyLog("test", buffer)
+		err := walFS.VfsApplyLog("test", logBuffer)
 		assert.NoError(t, err)
+
+		walFile, err := walFS.OpenFile("test", os.O_RDWR, 0660)
+		assert.NoError(t, err)
+
+		assert.Equal(t, true, walFile.hasWriteHeader)
+
+		assert.Equal(t, VFS__WAL_HEADER_SIZE, len(walFile.header))
+		for i := 0; i < len(walFile.header); i++ {
+			assert.NotEqual(t, 0, walFile.header[i])
+		}
+
+		assert.Equal(t, len(cmd.Frames), walFile.frames)
+
+		for index, frame := range cmd.Frames {
+			walFrame := walFile.frames[index]
+			assert.Equal(t, true, walFrame.hasWriteHeader)
+			assert.Equal(t, true, walFrame.hasWritePage)
+			assert.Equal(t, pageSize, walFrame.pageSize)
+
+			for i := 0; i < pageSize; i++ {
+				assert.Equal(t, frame.Data[i], walFile.frames[i])
+			}
+		}
 	})
 
 }
