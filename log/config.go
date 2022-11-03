0ae6943b274eb80d4bf27e266581245a93a236fe
uglyer <root@wanda.sb>
2022-11-03 22:43:35 +0800

    🚧 支持日志写入控制参数

--- log/config.go
+++ log/config.go
@@ -1,28 +1,28 @@
 package log
 
+import "path"
+
+// LogConfig 日志参数
+type LogConfig struct {
+	Path         string        `mapstructure:"path" yaml:"path"`
+	AccessOption *RotateOption `mapstructure:"access-option" yaml:"access-option"`
+	ErrorOption  *RotateOption `mapstructure:"error-option" yaml:"error-option"`
+}
+
 // SetDefaultLogMode 设置默认大日志模式
-func SetDefaultLogMode() {
+func SetDefaultLogMode(config *LogConfig) {
+	println(config.ErrorOption.MaxAge)
 	var tops = []TeeOption{
 		{
-			Filename: "access.log",
-			Ropt: RotateOption{
-				MaxSize:    1,
-				MaxAge:     1,
-				MaxBackups: 3,
-				Compress:   true,
-			},
+			Filename: path.Join(config.Path, "access.log"),
+			Ropt:     *config.AccessOption,
 			Lef: func(lvl Level) bool {
 				return lvl <= InfoLevel
 			},
 		},
 		{
-			Filename: "error.log",
-			Ropt: RotateOption{
-				MaxSize:    1,
-				MaxAge:     1,
-				MaxBackups: 3,
-				Compress:   true,
-			},
+			Filename: path.Join(config.Path, "error.log"),
+			Ropt:     *config.ErrorOption,
 			Lef: func(lvl Level) bool {
 				return lvl > InfoLevel
 			},
