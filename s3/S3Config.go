92fd72ea18fe812c5924e996c1a517414231b1eb
uglyer <root@wanda.sb>
2022-11-12 20:05:36 +0800

    🚧 补充参数

--- s3/S3Config.go
+++ s3/S3Config.go
@@ -2,10 +2,12 @@ package s3
 
 // S3Config S3 存储桶配置参数
 type S3Config struct {
+	Enabled    bool   `mapstructure:"enabled" yaml:"enabled"`
 	AccessKey  string `mapstructure:"access-key" yaml:"access-key"`
 	SecretKey  string `mapstructure:"secret-key" yaml:"secret-key"`
 	Endpoint   string `mapstructure:"endpoint" yaml:"endpoint"`
 	Region     string `mapstructure:"region" yaml:"region"`
 	DisableSSL bool   `mapstructure:"disable-ssl" yaml:"disable-ssl"`
 	Bucket     string `mapstructure:"bucket" yaml:"bucket"`
+	PrefixPath string `mapstructure:"prefix-path" yaml:"prefix-path"`
 }
